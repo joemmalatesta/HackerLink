fae5a488267684edd80ec81e46a8b06042587a99
joemmalatesta <joemmalatesta@gmail.com>
2024-06-02 01:43:59 -0700

    another way added. cool

--- src/routes/forms/[form]/+page.svelte
+++ src/routes/forms/[form]/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+    // This is how we do slugs server side
     export let data
     const { form } = data
+    
+    
+    // This is how we do slugs client side
+    // import {page} from '$app/stores'
+    // $: form = $page.params.form
 </script>
 
 {form}
